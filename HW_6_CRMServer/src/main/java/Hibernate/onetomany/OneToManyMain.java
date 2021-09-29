98d87427f98b7ab1eaa146a6bca889708a4688cb
leno4ka_tihanovich <leno4katihanovich@gmail.com>
2021-09-29 18:30:17 +0300

    Hibernate OneToManyTest

--- HW_6_CRMServer/src/main/java/Hibernate/onetomany/OneToManyMain.java
+++ HW_6_CRMServer/src/main/java/Hibernate/onetomany/OneToManyMain.java
@@ -56,6 +56,7 @@ public class OneToManyMain {
         System.out.println(getDepartmentById(depId2).toString());
 
         AppSessionFactory.shutDown();
+
     }
 
     public static void saveEmployer(Employer employer){
